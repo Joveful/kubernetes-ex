589f5fb7157f130ca11079aea4f570889f4b49e7
Jori Vehkakoski <j.vehkakoski@gmail.com>
2026-08-15 10:26:13 +0300

    deployment port env

--- the_project/src/index.ts
+++ the_project/src/index.ts
@@ -1,12 +1,16 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { html } from 'hono/html'
 
 const PORT = Number(process.env.PORT) || 3000
 
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.html(
+    html`<!doctype html>
+    <h1>Todo app</h1>`
+  )
 })
 
 serve({
